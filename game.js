7f9c140287b352874f304b0fcf8ef1c7f464d662
FireMiner99 <FireMiner99@users.noreply.github.com>
2016-09-08 15:07:00 -0700

    Update game.js

--- game.js
+++ game.js
@@ -9,6 +9,33 @@ var mycanvas = document.getElementById('mycanvas');
     var snake;
     var snakeSize = 10;
     var food;
+    
+    // Module pattern
+var drawModule = (function () { 
+    var bodySnake = function(x, y) {
+        // This is the single square
+        ctx.fillStyle = 'green';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+        // This is the border of the square
+        ctx.strokeStyle = 'darkgreen';
+        ctx.strokeRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+    }
+ 
+    var pizza = function(x, y) {
+        // This is the border of the pizza
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(x*snakeSize, y*snakeSize, snakeSize, snakeSize);
+        // This is the single square 
+        ctx.fillStyle = 'red';
+        ctx.fillRect(x*snakeSize+1, y*snakeSize+1, snakeSize-2, snakeSize-2);
+    }
+ 
+    var scoreText = function() {
+        // How many pizzas did the snake eat
+        var score_text = "Score: " + score;
+        ctx.fillStyle = 'blue';
+        ctx.fillText(score_text, 145, h-5);
+    }
 
 /*
 SNake, Area where snake moves, controls, some way of putting apples into the snake area, give commands to snake
