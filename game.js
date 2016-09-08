c7ff45cdc71610c4ad23d964195fc2904e4c14c9
FireMiner99 <FireMiner99@users.noreply.github.com>
2016-09-08 15:02:38 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 console.log("Welcome to the Console!!!")
 
-
+var mycanvas = document.getElementById('mycanvas');
+    var ctx = mycanvas.getContext('2d');
+    var snakeSize = 10; 
+    var w = 350;
+    var h = 350;
+    var score = 0;
+    var snake;
+    var snakeSize = 10;
+    var food;
 
 /*
 SNake, Area where snake moves, controls, some way of putting apples into the snake area, give commands to snake
